40527ed09d3a3e6d46898f210109bca11ccd820f
Dan Proctor <dproctor@chapman.edu>
2019-04-26 10:49:35 -0700

    Updated cars.sh

--- cars.sh
+++ cars.sh
@@ -2,4 +2,21 @@
 # cars.sh
 # Dan Proctor
 
-while 
+echo "Enter one of the following commands:"
+echo "\"Add\" adds a car to the inventory file"
+echo "\"List\" lists the cars in the inventory file"
+echo "\"Quit\" quits the program."
+echo "(Commands are case sensitive)"
+
+read input
+
+while [ $input -ne "Quit" ]
+do
+	case "$input" in
+	
+	
+	echo "Enter another command:"
+
+	
+
+ 
