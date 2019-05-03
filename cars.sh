b5ed608bd9ef378b318483c958496cde35b8c010
Dan Proctor <dproctor@chapman.edu>
2019-05-03 08:33:48 -0700

    Updated cars.sh

--- cars.sh
+++ cars.sh
@@ -3,19 +3,46 @@
 # Dan Proctor
 
 echo "Enter one of the following commands:"
-echo "\"Add\" adds a car to the inventory file"
-echo "\"List\" lists the cars in the inventory file"
-echo "\"Quit\" quits the program."
-echo "(Commands are case sensitive)"
+echo "1) Adds a car to the inventory file"
+echo "2) List the cars in the inventory file"
+echo "Q) Quit the program."
 
 read input
 
-while [ $input -ne "Quit" ]
+while [ $input != "Q" ]
 do
-	case "$input" in
+	
+	if [ $input == 1 ]
+	then
+		echo "What is the year?"
+		read year
+		echo "What is the make?"
+		read make
+		echo "What is the model?"
+		read model
+		echo $year:$make:$model >> My_old_cars
+	elif [ $input == 2 ]
+	then 
+		sort My_old_cars
+	elif [$input == "Q" ]
+	then 
+		echo "Goodbye"
+		break
+	else
+		echo "Please enter a valid option."
+	fi
+	
+	echo ""
+	echo "Please enter another command:"
+	echo "1) Add a car to the inventory file"
+	echo "2) List the cars in the inventory file"
+	echo "Q) Quit the program."
+	read input
+
+
+done
 	
 	
-	echo "Enter another command:"
 
 	
 
